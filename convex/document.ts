09e9c8d2e26f248344635c0cb49a4749d8390207
aliallam98 <allam98.ali@gmail.com>
2024-01-10 21:52:56 +0200

    added update api function to use it later to update doc info

--- convex/document.ts
+++ convex/document.ts
@@ -241,3 +241,34 @@ export const getById = query({
     return isDocExisting
   }
 })
+
+export const updateById = mutation({
+  args:{
+    id: v.id("documents"),
+    title:v.optional(v.string()),
+    isArchived:v.optional(v.boolean()),
+    isPublished:v.optional(v.boolean()),
+    icon:v.optional(v.string()),
+    coverImage:v.optional(v.string()),
+    content:v.optional(v.string()),
+  },
+  handler: async (ctx, args)=> {
+    const identity = await ctx.auth.getUserIdentity()
+    const userId = identity?.subject
+    if(!identity){
+      throw new Error("Not Authenticated");
+    }
+    const isDocExisting = await ctx.db.get(args.id)
+    if(!isDocExisting){
+      throw new Error("Cannot Find This Doc");
+    }
+    if(isDocExisting.userId !== userId){
+      throw new Error("Not Authenticated");
+    }
+    const {_id,...rest} = isDocExisting
+    const document = await ctx.db.patch(args.id,{
+      ...rest
+    })
+    return document
+  },
+})
